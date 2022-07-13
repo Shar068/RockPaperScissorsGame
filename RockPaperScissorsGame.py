a001a87ba992ccce48470357a34fd1536662f158
Shar068 <108710778+Shar068@users.noreply.github.com>
2022-07-13 10:46:12 +0530

    Add files via upload

---
+++ RockPaperScissorsGame.py
@@ -0,0 +1,43 @@
+#rock,paper,scissors game
+
+import random
+
+choices=["rock","paper","scissors"]
+computer=random.choice(choices)
+player=None
+
+while player not in choices :
+  player=input("rock,paper,scissors? : ").lower()
+if player == computer :
+  print("computer : ",computer)
+  print("player : ",player)
+  print("Tie!")
+elif player == "rock" :
+  if computer == "paper" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You lose!")
+  if computer == "scissors" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You win!")
+
+elif player == "scissors" :
+  if computer == "paper" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You win!")
+  if computer == "rock" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You lose!")
+
+elif player == "paper" :
+  if computer == "scissors" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You lose!")
+  if computer == "rock" :
+    print("computer : ",computer)
+    print("player : ",player)
+    print("You win!")
